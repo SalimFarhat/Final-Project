f4aca581d0af499cbe1d323ca04fae28176a323a
Salim Farhat <salimfarhat@gmail.com>
2022-06-10 14:31:05 -0400

    Only Calendar is needed now

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -22,16 +22,46 @@ const HomePage = () => {
     function handleCallbackResponse(response){
         // console.log("Encoded JWT ID token: " + response.credential);
         const userObject = jwt_decode(response.credential);
-        console.log(userObject.email);
-        setUser({email: userObject.email})
-        if(userObject.email === 'salimfarhat@gmail.com'){
-            setAdminSignedIn(true);
+        console.log(userObject);
+        console.log(userObject.given_name);
+        if(userObject.email === "salimfarhat@gmail.com"){
+          setAdminSignedIn(true);
+          setSignedIn(false);
+          setUser({email: userObject.email})
         }else{
-            setSignedIn(true);
-        }
+        fetch(`client/${userObject.email}`)
+        .then(res => res.json())
+        .then(data => {
+          if(data.data === null){
+            fetch(`/add-client`,{
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+              },
+              body: JSON.stringify({
+                _id: userObject.email,
+                email: userObject.email,
+                name: userObject.given_name,
+              })
+            })
+            console.log(userObject.given_name + " Has been added to the client base!")
+          }else{
+            
+          setAdminSignedIn(false)
+          setSignedIn(true)
+          setUser({email: userObject.email})
+          }
+        }).catch(err => console.log(err))}
+        // setUser({email: userObject.email})
+        // if(userObject.email === 'salimfarhat@gmail.com'){
+        //     setAdminSignedIn(true);
+        // }else{
+        //     setSignedIn(true);
+        // }
     
       }
-      console.log(user);
+      // console.log(user);
     
       useEffect(() => {
         /* global google */
